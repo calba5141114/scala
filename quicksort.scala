7ee1ba321f2c9fec78f37648dd1f0429bc9e9eb4
Carlos Alba <soradev4@gmail.com>
2019-06-09 12:28:04 -0700

    Turning standalone function into an Object

--- quicksort.scala
+++ quicksort.scala
@@ -1,9 +1,19 @@
-def sort(xs: Array[Int]): Array[Int] = {
-  if (xs.length <= 1) xs
-  else {
-    val pivot = xs(xs.length / 2)
-    Array.concat(sort(xs filter (pivot >)),
-                 xs filter (pivot ==),
-                 sort(xs filter (pivot <)))
+object Sorter {
+
+  def sort(xs: Array[Int]): Array[Int] = {
+    if (xs.length <= 1) xs
+    else {
+      
+      val pivot = xs(xs.length / 2)
+      Array.concat(sort(xs filter (pivot >)),
+                   xs filter (pivot ==),
+                   sort(xs filter (pivot <)))
+    }
   }
+
+  def main(args: Array[String]): Unit = {
+    var ls = Array(10, 3, 4, 54, 67, 100, 123)
+    println(sort(ls))
+  }
+  
 }
