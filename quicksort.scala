46972f1de5881787ed37cff7e4273cccceddd8ec
Carlos Alba <soradev4@gmail.com>
2019-06-09 12:31:17 -0700

    Convert to String

--- quicksort.scala
+++ quicksort.scala
@@ -1,9 +1,11 @@
+import scala.language.postfixOps
+
 object Sorter {
 
   def sort(xs: Array[Int]): Array[Int] = {
     if (xs.length <= 1) xs
     else {
-      
+
       val pivot = xs(xs.length / 2)
       Array.concat(sort(xs filter (pivot >)),
                    xs filter (pivot ==),
@@ -13,7 +15,8 @@ object Sorter {
 
   def main(args: Array[String]): Unit = {
     var ls = Array(10, 3, 4, 54, 67, 100, 123)
-    println(sort(ls))
+    val sorted = sort(ls)
+    println(sorted.mkString(" "))
   }
-  
+
 }
